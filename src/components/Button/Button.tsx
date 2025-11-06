b8e0df0cdd2f298fb9d940c7021fe8a13d463d6b
ItsGreatPro <karahan@academic.rrc.ca>
2025-11-05 21:57:56 -0600

    test'

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ButtonProps  from './Button.types';
+import  ButtonProps  from './Button.types';
 
 const StyledButton = styled.button<{
   backgroundColor?: string;
